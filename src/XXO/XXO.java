6a9f6d3f87a016444874727a5853f084533c39d8
andreas <rendler@logicsale.de>
2016-06-16 11:40:10 +0200

    test from another pc

--- src/XXO/XXO.java
+++ src/XXO/XXO.java
@@ -3,4 +3,12 @@ package XXO;
 
 public class XXO
 {
+	public XXO()
+	{
+	}
+
+	public boolean isVictory()
+	{
+		return true;
+	}
 }
--- src/XXO/XXOTest.java
+++ src/XXO/XXOTest.java
@@ -1,12 +1,20 @@
 package XXO;
 
 import org.junit.Before;
-
+import org.junit.Test;
 
 public class XXOTest
 {
+	private XXO classUnderTest = new XXO();
+
 	@Before
 	public void setUp() throws Exception
+	{
+
+	}
+
+	@Test
+	public void testIsVictory() throws Exception
 	{
 
 	}
