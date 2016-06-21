e487a6aa3e80f1ed2ff27ad36111cb337489d89f
andreas <rendler@logicsale.de>
2016-06-21 10:42:25 +0200

    added first tests and logic

--- src/XXO/XXO.java
+++ src/XXO/XXO.java
@@ -1,14 +1,55 @@
 package XXO;
 
+import java.util.Objects;
 
-public class XXO
+class XXO
 {
-	public XXO()
+	boolean isVictory(Integer[][] matrix, Integer playerValue)
 	{
+		return false;
 	}
 
-	public boolean isVictory()
+	Boolean isVictoryHorizontal(Integer[][] matrix, Integer playerValue)
 	{
-		return true;
+		Boolean isVictory = true;
+
+		for (Integer[] firstRow : matrix)
+		{
+			isVictory = true;
+
+			for (int i = 0; i < firstRow.length-1; i++)
+			{
+				isVictory &= (Objects.equals(firstRow[i], playerValue));
+			}
+
+			if(isVictory)
+			{
+				break;
+			}
+
+		}
+		return isVictory;
+	}
+
+	public Boolean isVictoryVertical(Integer[][] matrix, Integer playerValue)
+	{
+		Boolean isVictory = true;
+
+		for (int i = 0; i < matrix.length; i++)
+		{
+			isVictory = true;
+
+			for (int j = 0; j < matrix.length; j++)
+			{
+				isVictory &= (Objects.equals(matrix[j][i], playerValue));
+			}
+
+			if(isVictory)
+			{
+				break;
+			}
+
+		}
+		return isVictory;
 	}
 }
--- src/XXO/XXOTest.java
+++ src/XXO/XXOTest.java
@@ -1,22 +1,117 @@
 package XXO;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+
 public class XXOTest
 {
-	private XXO classUnderTest = new XXO();
+	private XXO classUnderTest;
 
 	@Before
 	public void setUp() throws Exception
 	{
-
+		classUnderTest = new XXO();
 	}
 
 	@Test
-	public void testIsVictory() throws Exception
+	public void testIsVictoryHorizontalWhenPlayerOneHasFirstRowReturnTrue() throws Exception
 	{
+		Integer[][] matrix = {
+				{1,1,1},
+				{0,0,0},
+				{0,0,0}
+		};
 
+		Integer playerValue = 1;
+
+		Boolean isVictory = classUnderTest.isVictoryHorizontal(matrix, playerValue);
+
+		Assert.assertThat(isVictory, is(true));
+	}
+
+	@Test
+	public void testIsVictoryHorizontalWhenPlayerOneHasLastRowReturnTrue() throws Exception
+	{
+		Integer[][] matrix = {
+				{1,0,1},
+				{0,0,0},
+				{2,2,2}
+		};
+
+		Integer playerValue = 2;
+
+		Boolean isVictory = classUnderTest.isVictoryHorizontal(matrix, playerValue);
+
+		Assert.assertThat(isVictory, is(true));
+	}
+
+	@Test
+	public void testIsVictoryVerticalWhenPlayerOneHasFirstRowReturnTrue() throws Exception
+	{
+		Integer[][] matrix = {
+				{1,1,1},
+				{1,0,0},
+				{1,0,0}
+		};
+
+		Integer playerValue = 1;
+
+		Boolean isVictory = classUnderTest.isVictoryVertical(matrix, playerValue);
+
+		Assert.assertThat(isVictory, is(true));
+	}
+
+	@Test
+	public void testIsVictoryVerticalWhenPlayerOneHasLastRowReturnTrue() throws Exception
+	{
+		Integer[][] matrix = {
+				{1,0,2},
+				{0,0,2},
+				{2,2,2}
+		};
+
+		Integer playerValue = 2;
+
+		Boolean isVictory = classUnderTest.isVictoryVertical(matrix, playerValue);
+
+		Assert.assertThat(isVictory, is(true));
+	}
+
+
+
+	@Test
+	public void testIsVictoryWhenAllFieldsTrue() throws Exception
+	{
+		Integer[][] matrix = {
+				{0,0,0},
+				{0,0,0},
+				{0,0,0}
+		};
+
+		Integer playerValue = 1;
+
+		Boolean isVictory = classUnderTest.isVictory(matrix, playerValue);
+
+		Assert.assertThat(isVictory, is(false));
+	}
+
+	@Test
+	public void testIsVictoryWhenFirstRowPlayerOne() throws Exception
+	{
+		Integer[][] matrix = {
+				{1,1,1},
+				{2,2,2},
+				{0,0,0}
+		};
+
+		Integer playerValue = 1;
+
+		Boolean isVictory = classUnderTest.isVictory(matrix, playerValue);
+
+		Assert.assertThat(isVictory, is(true));
 	}
 
 }
